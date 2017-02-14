3c4e8eefce0af75311d77fc2f265eef8bbc24397
mulin <316428218@qq.com>
2017-02-14 20:20:06 +0800

    add

---
+++ start/src/main/java/com/boot/rest/invoker.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Administrator on 2017/2/12 0012.
+ */
+package com.boot.rest;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@EnableAutoConfiguration
+@RestController
+public class invoker {
+
+    @ResponseBody
+    @RequestMapping(value="/")
+    String location(){
+        return "北京";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/{name}/{age}")
+    User User(@PathVariable(name = "name") String  name, @PathVariable(name = "age") int age){
+        User u=new User();
+        u.setName(name);
+        u.setAge(age);
+        return  u;
+    }
+}
+
+class  User{
+
+    private  int age;
+
+    private  String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
