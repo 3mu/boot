efc1c7a17fbc7f30a2ebaaf4915ff9f58c7feb80
mulin <316428218@qq.com>
2017-02-16 23:02:15 +0800

    111

--- start/web/src/main/java/com/boot/web/WebApplication.java
+++ start/web/src/main/java/com/boot/web/WebApplication.java
@@ -4,11 +4,23 @@
 package com.boot.web;
 
 import com.boot.service.ServiceApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class WebApplication {
 
-    public  static  void main(String[] agrs){
-     String message=  new ServiceApplication().getHello();
-     System.out.print(message);
- }
+    public  static  void main(String[] args){
+
+        SpringApplication.run(WebApplication.class,args);
+    }
+   public String webHello(){
+
+       String message=  new ServiceApplication().getHello();
+       return  String.format("from web {0}",message);
+   }
 }
