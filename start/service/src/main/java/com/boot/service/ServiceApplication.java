f1d836fd8260c69631842abb73771539044a87ec
mulin <316428218@qq.com>
2017-02-19 21:47:32 +0800

    vs

--- start/service/src/main/java/com/boot/service/ServiceApplication.java
+++ start/service/src/main/java/com/boot/service/ServiceApplication.java
@@ -6,21 +6,14 @@ package com.boot.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@RestController
+@ComponentScan(basePackages = "com.boot")
 public class ServiceApplication {
 
-    public  static  void  main(String[] args){
-
-        SpringApplication.run(ServiceApplication.class,args);
-
-    }
-    @RequestMapping
-    public String getHello(){
-        return  "hello this is my code";
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
     }
 }
