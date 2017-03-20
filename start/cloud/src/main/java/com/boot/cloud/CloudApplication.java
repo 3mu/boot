85f64ccb8a3d58ce2db921f4406f3978b7239875
mulin <316428218@qq.com>
2017-03-20 09:52:18 +0800

    ``

--- start/cloud/src/main/java/com/boot/cloud/CloudApplication.java
+++ start/cloud/src/main/java/com/boot/cloud/CloudApplication.java
@@ -1,17 +1,15 @@
 /**
  * Created by Administrator on 2017/3/19 0019.
  */
-package com.boot.clond;
+package com.boot.cloud;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudApplication {
 
     public static void main(String[] args) {
