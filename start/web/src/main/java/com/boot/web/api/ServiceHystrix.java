709ffe432a9f9d8d52e058e5906aef5dd234705e
mulin <316428218@qq.com>
2017-03-23 17:05:06 +0800

    api gateway

--- start/web/src/main/java/com/boot/web/api/ServiceHystrix.java
+++ start/web/src/main/java/com/boot/web/api/ServiceHystrix.java
@@ -1,5 +1,7 @@
 package com.boot.web.api;
 
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +10,13 @@ public  class  ServiceHystrix implements  ServerClient{
 
     @Override
     public String add() {
+
+////        com.netflix.loadbalancer.
+//        ZoneAwareLoadBalancer<Server> lb = (ZoneAwareLoadBalancer<Server>) springClientFactory.getLoadBalancer("CLOUD-SERVICE");
+//        Server server = lb.chooseServer();
+//        System.out.println("error->" + server.getHostPort());
+//        lb.markServerDown(server);
+
         return "this  services is error";
     }
 }
---
+++ start/zuul/src/main/java/com/boot/zuul/filter/AccessFilter.java
@@ -0,0 +1,43 @@
+package com.boot.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2017/3/23 0023.
+ */
+
+@Component
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            ctx.setResponseBody("access token is not null");
+            return "access token is not null";
+        }
+        return null;
+    }
+}
